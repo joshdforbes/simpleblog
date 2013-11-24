22c209cb8cd22ccc1bbf67c4304474486d417ad8
joshdforbes <jdforbes82@gmail.com>
2013-11-23 21:48:52 -0500

    index file modified to test database connection

--- public/index.php
+++ public/index.php
@@ -1,21 +1,6 @@
 <?php 
-//namespace simpleblog;
+include '../config.php';
+include '../classes/Logger.class.php';
+include '../classes/DatabaseConnection.class.php';
 
-function __autoload($className)
-{
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-	
-	echo $fileName;
-    require $fileName;
-}
-
-$test = new Test();
-
+$dbh = new DatabaseConnection($config);
