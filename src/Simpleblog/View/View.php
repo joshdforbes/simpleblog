df48cd517eb5bf4c5723f8e9f47d27950618d770
joshdforbes <jdforbes82@gmail.com>
2014-02-28 13:53:07 -0500

    Work on how the framework handles Exceptions

--- src/Simpleblog/View/View.php
+++ src/Simpleblog/View/View.php
@@ -14,7 +14,15 @@ class View
 	}
 
 	public function render($template) {
-		require($template);
+		$path = $_SERVER['DOCUMENT_ROOT'] . '/assets/templates/' . $template;
+		if (file_exists($path)){
+			ob_start();
+			require($path);
+			return ob_get_clean();
+		} else {
+			Throw new \Exception('Template not found');
+		}
+		
 	}
 
 
