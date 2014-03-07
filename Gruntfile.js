8f4588e77e710c5ac15ede03412bf819ed83c71b
joshdforbes <jdforbes82@gmail.com>
2014-03-07 13:14:27 -0500

    Implementing Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'public/js/libs/*.js',
+                    'public/js/global.js'
+                ],
+                dest: 'public/js/build/production.js',
+            }
+        }
+
+    });
+
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+   
+    grunt.registerTask('default', ['concat']);
+
+};
