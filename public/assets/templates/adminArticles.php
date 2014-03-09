773721f5a2d7aac71f8efce78961cf3ea67387e9
joshdforbes <jdforbes82@gmail.com>
2014-03-08 20:54:15 -0500

    Added bower to project

--- public/assets/templates/adminArticles.php
+++ public/assets/templates/adminArticles.php
@@ -3,17 +3,19 @@
 	<title>
 		<?php echo $this->data['title']; ?>
 	</title>
+	<link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	
 
-
 <?php foreach($this->data['articles'] as $article): ?>
+	<article>
 	<h1><a href="/articles/article/<?= $article->id; ?>"> <?= $article->title; ?> </a></h1>
 	
     <h5><?= $article->date; ?></h5>
     <p><?= $article->contentPreview; ?></p>
     <a href="/admin/deleteArticle/<?= $article->id; ?>">DELETE</a>
+	</article>
 <?php endforeach; ?>
 
 <form action="/admin/insertArticle" method="post">
